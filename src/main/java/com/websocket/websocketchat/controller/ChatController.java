5efd6b78b93bfa2ebc41c283ccf4670644618132
kyoun9min <rtong77@naver.com>
2022-02-16 18:03:08 +0900

    basic websocket server

---
+++ src/main/java/com/websocket/websocketchat/controller/ChatController.java
@@ -0,0 +1,30 @@
+package com.websocket.websocketchat.controller;
+
+import com.websocket.websocketchat.dto.ChatRoom;
+import com.websocket.websocketchat.service.ChatService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/chat")
+public class ChatController {
+
+    private final ChatService chatService;
+
+    @PostMapping
+    public ChatRoom createRoom(@RequestParam String name) {
+        return chatService.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRoom() {
+        return chatService.findAllRoom();
+    }
+
+}
